32cd2856b7e6d9ab5a855ba69c0ca83866e8b2dc
Eli <elipinals12@gmail.com>
2023-02-27 12:14:10 -0500

    Update xkcdpwgen.c

--- project3/xkcdpwgen.c
+++ project3/xkcdpwgen.c
@@ -9,22 +9,22 @@
 // void function to print full help message
 void print_help()
 {
-    printf("usage: xkcdpwgen [-h] [-w WORDS] [-c CAPS] [-n NUMBERS] [-s SYMBOLS]
-                
-Generate a secure, memorable password using the XKCD method
-                
-optional arguments:
-    -h, --help            show this help message and exit
-    -w WORDS, --words WORDS
-                          include WORDS words in the password (default=4)
-    -c CAPS, --caps CAPS  capitalize the first letter of CAPS random words
-                          (default=0)
-    -n NUMBERS, --numbers NUMBERS
-                          insert NUMBERS random numbers in the password
-                          (default=0)
-    -s SYMBOLS, --symbols SYMBOLS
-                          insert SYMBOLS random symbols in the password
-                          (default=0)");
+    printf("usage: xkcdpwgen [-h] [-w WORDS] [-c CAPS] [-n NUMBERS] [-s SYMBOLS]\n\n");
+
+    printf("Generate a secure, memorable password using the XKCD method\n\n");
+
+    printf("optional arguments:");
+    printf("    -h, --help            show this help message and exit\n");
+    printf("    -w WORDS, --words WORDS\n");
+    printf("                          include WORDS words in the password (default=4)\n");
+    printf("    -c CAPS, --caps CAPS  capitalize the first letter of CAPS random words\n");
+    printf("                          (default=0)\n");
+    printf("    -n NUMBERS, --numbers NUMBERS\n");
+    printf("                          insert NUMBERS random numbers in the password\n");
+    printf("                          (default=0)\n");
+    printf("    -s SYMBOLS, --symbols SYMBOLS\n");
+    printf("                          insert SYMBOLS random symbols in the password\n");
+    printf("                          (default=0)");
 }
 
 // function to generate a random integer between min and max (inclusive)
